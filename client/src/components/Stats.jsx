59dd0a242360bdbabc74c4a02c348874a510ef20
jedtherooster <55644921+jedtherooster@users.noreply.github.com>
2026-07-22 09:24:28 +1200

    Add statistics page frontend skeleton

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import ArrowUpIcon from "../assets/arrow-up-right.svg?react";
 
 function Statistics() {
   return (
     <>
-      <h1>Statistics</h1>
+      <div className="stats-container">
+        <div className="stats-title-button-container">
+          <div className="stats-title">
+            <h2>Statistics</h2>
+            <p>See how well your vehicle is performing</p>
+          </div>
+          <div className="stats-button button">
+            <p className="stats-plus-icon">+</p>
+            <p>Add Purchase</p>
+          </div>
+        </div>
+        <div className="stats-card">
+          <ArrowUpIcon className="icon"></ArrowUpIcon>
+          <h6>No fuel purchases logged yet</h6>
+          <p>Start tracking your fuel spending to see trends and insights</p>
+        </div>
+      </div>
     </>
   );
 }
