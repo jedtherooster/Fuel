e6aad67c76097a10567cf068f0e3ec7f99abf705
jedtherooster <55644921+jedtherooster@users.noreply.github.com>
2026-07-22 13:46:38 +1200

    Add vehicles page

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -4,18 +4,18 @@ import ArrowUpIcon from "../assets/arrow-up-right.svg?react";
 function Statistics() {
   return (
     <>
-      <div className="stats-container">
-        <div className="stats-title-button-container">
-          <div className="stats-title">
+      <div className="card-container">
+        <div className="card-title-button-container">
+          <div className="card-title">
             <h2>Statistics</h2>
             <p>See how well your vehicle is performing</p>
           </div>
-          <div className="stats-button button">
-            <p className="stats-plus-icon">+</p>
+          <div className="card-button button">
+            <p className="card-plus-icon">+</p>
             <p>Add Purchase</p>
           </div>
         </div>
-        <div className="stats-card">
+        <div className="card">
           <ArrowUpIcon className="icon"></ArrowUpIcon>
           <h6>No fuel purchases logged yet</h6>
           <p>Start tracking your fuel spending to see trends and insights</p>
--- client/src/components/Vehicles.jsx
+++ client/src/components/Vehicles.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import VehicleIcon from "../assets/car-front-fill.svg?react";
 
 function Vehicles() {
   return (
-    <>
-      <h1>Vehicles</h1>
-    </>
+    <div className="card-container">
+      <div className="card-title-button-container">
+        <div className="card-title">
+          <h2>Vehicles</h2>
+          <p>Manage your veheicles</p>
+        </div>
+        <div className="card-button button">
+          <p className="card-plus-icon">+</p>
+          <p>Add Vehicle</p>
+        </div>
+      </div>
+      <div className="card">
+        <VehicleIcon className="icon"></VehicleIcon>
+        <h6>No current vehicles</h6>
+        <p>Manage veheicle fuel consumption</p>
+      </div>
+    </div>
   );
 }
 
